0f9ba0cfabe160d19674906afdb4eed918bd3b0f
bagiyoni <yonatan.b@onoapps.com>
2019-02-04 10:03:38 +0200

    Fix accessibility in recyclerView (staggered) that the focus will move in order.

--- app/src/main/java/com/onoapps/staggeredgridforaccessibility/AdapterStaggeredGrid.java
+++ app/src/main/java/com/onoapps/staggeredgridforaccessibility/AdapterStaggeredGrid.java
@@ -1,6 +1,10 @@
 package com.onoapps.staggeredgridforaccessibility;
 
 import android.support.annotation.NonNull;
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,22 +12,35 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 /**
  * Created by Yonatan Bagizada on 29/01/2019.
  */
 public class AdapterStaggeredGrid extends RecyclerView.Adapter<AdapterStaggeredGrid.MyViewHolder> {
 
-    int[] imgList = {R.drawable.ic_computer,
+    int[] imgList = {
+            R.drawable.ic_computer,
             R.drawable.ic_delivery,
             R.drawable.ic_graph,
             R.drawable.ic_present,
             R.drawable.ic_shopping,
             R.drawable.ic_stadistics,
-            R.drawable.ic_wallet
+            R.drawable.ic_wallet,
+            R.drawable.ic_present,
+            R.drawable.ic_graph,
+            R.drawable.ic_wallet,
+            R.drawable.ic_shopping,
+            R.drawable.ic_stadistics,
+            R.drawable.ic_graph,
+            R.drawable.ic_delivery,
+            R.drawable.ic_computer,
     };
 
     String[] nameList = {"One", "Two", "Three", "Four", "Five", "Six",
-            "Seven", "Eight", "Nine", "Ten"};
+            "Seven", "Eight", "Nine", "Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen"};
+
+    HashMap<Integer, CardView> mCardViewHashMap = new HashMap<>();
 
     @NonNull
     @Override
@@ -34,9 +51,19 @@ public class AdapterStaggeredGrid extends RecyclerView.Adapter<AdapterStaggeredG
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         holder.mImageView.setImageResource(imgList[position]);
         holder.mTextView.setText(nameList[position]);
+            mCardViewHashMap.put(position,holder.mCardView);
+            if (mCardViewHashMap.size() > 1){
+                ViewCompat.setAccessibilityDelegate(mCardViewHashMap.get(position), new AccessibilityDelegateCompat(){
+                    @Override
+                    public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfoCompat info) {
+                        super.onInitializeAccessibilityNodeInfo(host, info);
+                        info.setTraversalAfter(mCardViewHashMap.get(position-1));
+                    }
+                });
+            }
     }
 
     @Override
@@ -47,10 +74,12 @@ public class AdapterStaggeredGrid extends RecyclerView.Adapter<AdapterStaggeredG
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextView;
         public ImageView mImageView;
+        public CardView mCardView;
         public MyViewHolder(View v) {
             super(v);
             mTextView = v.findViewById(R.id.tvItem);
             mImageView = v.findViewById(R.id.ivItem);
+            mCardView = v.findViewById(R.id.cv);
         }
     }
 }
--- app/src/main/java/com/onoapps/staggeredgridforaccessibility/MainActivity.java
+++ app/src/main/java/com/onoapps/staggeredgridforaccessibility/MainActivity.java
@@ -1,25 +1,35 @@
 package com.onoapps.staggeredgridforaccessibility;
 
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeProviderCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerViewAccessibilityDelegate;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     AdapterStaggeredGrid mAdapterStaggeredGrid;
     StaggeredGridLayoutManager mStaggeredGridLayoutManager;
+    int count;
+    int sizeList;
+    boolean alreadyRun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAdapterStaggeredGrid = new AdapterStaggeredGrid();
         mRecyclerView = findViewById(R.id.rv);
         mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mStaggeredGridLayoutManager);
-        mAdapterStaggeredGrid = new AdapterStaggeredGrid();
         mRecyclerView.setAdapter(mAdapterStaggeredGrid);
-
+        sizeList = mAdapterStaggeredGrid.getItemCount();
     }
 }
